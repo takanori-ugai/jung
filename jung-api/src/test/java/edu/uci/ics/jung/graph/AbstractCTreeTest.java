7acb6aa8485384a7a7d1a7ccb9529202f07719b1
Takanori Ugai <ugai@fujitsu.com>
2025-04-15 16:49:40 +0900

    Update AbstractCTreeTest.java

--- jung-api/src/test/java/edu/uci/ics/jung/graph/AbstractCTreeTest.java
+++ jung-api/src/test/java/edu/uci/ics/jung/graph/AbstractCTreeTest.java
@@ -10,7 +10,7 @@
 
 package edu.uci.ics.jung.graph;
 
-import static com.google.common.truth.Truth8.assertThat;
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 import static edu.uci.ics.jung.graph.TestUtil.FAIL_ERROR_ELEMENT_NOT_IN_TREE;
 import static edu.uci.ics.jung.graph.TestUtil.FAIL_ERROR_NODEU_NOT_IN_TREE;
